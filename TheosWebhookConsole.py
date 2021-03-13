85417376e8dae36efac90d1c723922ad61809b9d
Theo <79630117+TheoTheEpic@users.noreply.github.com>
2021-03-13 19:55:49 +0000

    V1.6

--- TheosWebhookConsole.py
+++ TheosWebhookConsole.py
@@ -8,33 +8,34 @@ print("Loaded.")
 
 def main():
     while True:
-        print("Command list (Just type a number and press enter):")
+        print("Command list:")
         option = input('''
-        [0] Sets the webhook URL
-        [1] Webhook message spammer
-        [2] Delete webhook
-        [3] Show webhook info
-        [4] Change webhook name
-        [5] Say one message
-        [6] Checks the version
-        [7] Posts an ad for this webhook console
-        [8] Pings everyone... :troll:
-        [9] Sends sus!1!!!
+        [set] Sets the webhook URL
+        [spam] Webhook message spammer
+        [delete] Delete webhook
+        [info] Show webhook info
+        [change-name] Change webhook name
+        [say] Say one message
+        [version] Checks the version
+        [ad] Posts an ad for this webhook console
+        [everyone] Pings everyone... :troll:
+        [sus] Sends sus!1!!!
+        [rick] Sends a rickroll with a bit.ly link
         ==> ''')
 
-        version = "V1.5"
+        version = "V1.6"
 
-        if option == "0":
+        if option == "set":
             hook = input("Webhook URL: ")
-            data = {"content": "Success, connected to this webhook. Thanks for using Theo's Webhook Console by TheoTheEpic#0069"}
+            data = {"content": "Console message (Ignore): Logged into *this* webhook. (Webhook Console by TheoTheEpic#0069)"}
             headers = {"content-type": "application/json"}
             print("Setting the webhook url to: ",hook)
             time.sleep(0.5)
             r = requests.post(hook, json=data,headers=headers)
-            print("Success! Connected to the webhook: ",hook)
+            print("Success! Connected to the webhook, you can now use commands!")
 
         
-        if option == "1":
+        if option == "spam":
             message = input("Message to spam: ")
             amount = int(input("Amount to spam: "))
             wait = float(input("Message delay: "))
@@ -51,27 +52,27 @@ def main():
                 if r.status_code == 200:
                     print("Done")
 
-        if option == "2":
+        if option == "delete":
            
             requests.delete(hook)
             print("Deleting...")
             time.sleep(0.5)
             print("The requested webhook has been deleted.\n") 
 
-        if option == "3":
+        if option == "info":
             r = requests.get(hook)
             print("Getting webhook info...")
             time.sleep(0.5)
             print(r.content)
                 
-        if option == "4":
+        if option == "change-name":
             wbname = input("Set name: ")
             print("Processing...")
             time.sleep(0.5)
             requests.patch(hook, json={"name": wbname})
             print("Succsesfully changed webhook name to: ",wbname)
 
-        if option == "5":
+        if option == "say":
             message = input("Message to say: ")
             headers = {"content-type": "application/json"}
             data = {"content": message}
@@ -80,12 +81,12 @@ def main():
             r = requests.post(hook, json=data,headers=headers)
             print("Sent!")
         
-        if option == "6":
+        if option == "version":
             print("Loading version...")
             time.sleep(0.5)
             print("Version: ",version," (You may need to download the latest version at https://github.com/TheoTheEpic/Theos-Webhook-Console)")
 
-        if option == "7":
+        if option == "ad":
             data = {"content": "Get Theo's webhook console today! At: https://github.com/TheoTheEpic/Theos-Webhook-Console"}
             headers = {"content-type": "application/json"}
             print("Posting ad...")
@@ -93,7 +94,7 @@ def main():
             r = requests.post(hook, json=data,headers=headers)
             print("Posted")
 
-        if option == "8":
+        if option == "everyone":
             data = {"content": "@everyone :sunglasses:"}
             headers = {"content-type": "application/json"}
             print("Sending @everyone ping...")
@@ -101,7 +102,7 @@ def main():
             r = requests.post(hook, json=data,headers=headers)
             print("Sent @everyone ping!")
 
-        if option == "9":
+        if option == "sus":
          data = {"content": "When the imposter is sus :flushed:"}
          data2 = {"content": "https://media.discordapp.net/attachments/804662853059608627/820368810942398464/sus2.png"}
          headers = {"content-type": "application/json"}
@@ -110,6 +111,14 @@ def main():
          r = requests.post(hook, json=data,headers=headers)
          r = requests.post(hook, json=data2,headers=headers)
          print("Sent sus :flushed:")
+
+        if option == "rick":
+            data = {"content": "Come watch this epic video! :money_mouth: <http://bit.ly/pog-video>"}
+            headers = {"content-type": "application/json"}
+            print("Sending rickroll...")
+            time.sleep(0.5)
+            r = requests.post(hook, json=data,headers=headers)
+            print("Sent rickroll!")
 
 
 if __name__ == "__main__":
